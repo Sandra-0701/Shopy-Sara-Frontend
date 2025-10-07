765c36df3d96a9b9dbb0dd1e54629d6976d041b8
Sandra-0701 <sandrasindhu0701@gmail.com>
2025-10-07 18:31:11 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './index.css'
+function App() {
+  
+
+  return (
+    <div className="p-3 mb-4 bg-green-100 text-green-700 rounded-md">
+     you! Your message has been sent successfully.
+    </div>
+  )
+}
+
+export default App
